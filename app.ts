7a893ddcd679124b06029e3a86c6e381d20b87ca
Shaan Singh <ss5432@outlook.com>
2025-01-04 17:01:01 +0530

    Added static middleware and made a styles file to isolate styling.

--- app.ts
+++ app.ts
@@ -1,26 +1,45 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+const morgan = require("morgan");
+
 app.set("view engine", "ejs");
 
+app.listen(3000);
+
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
   console.log(req.url, req.method);
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
   console.log(req.url, req.method);
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create" });
   console.log(req.url, req.method);
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "Not Found" });
   console.log(req.url, req.method);
 });
-
-app.listen(3000);
