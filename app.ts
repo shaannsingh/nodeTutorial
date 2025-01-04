f1ad83ad83ddfd792c8d65a27cfcd573581cba00
Shaan Singh <ss5432@outlook.com>
2025-01-04 13:53:02 +0530

    Stepping foot into EJS, learning how render method works.

--- app.ts
+++ app.ts
@@ -1,24 +1,25 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
   console.log(req.url, req.method);
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
   console.log(req.url, req.method);
 });
 
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
   console.log(req.url, req.method);
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
   console.log(req.url, req.method);
 });
 
