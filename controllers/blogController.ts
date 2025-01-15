87cc80a48202e74c5e31a23419037142a693af43
Shaan Singh <ss5432@outlook.com>
2025-01-15 15:42:43 +0530

    Created blogs subfolder in views for blog specific views.

--- controllers/blogController.ts
+++ controllers/blogController.ts
@@ -5,7 +5,7 @@ const blogIndex = (req: Request, res: Response) => {
   Blog.find()
     .sort({ createdAt: -1 }) //descending order
     .then((result: any) => {
-      res.render("index", { title: "All Blogs", blogs: result });
+      res.render("blogs/index", { title: "All Blogs", blogs: result });
     })
     .catch((err: Error) => {
       console.log(err);
@@ -16,7 +16,7 @@ const blogDetails = (req: Request, res: Response) => {
   const id = req.params.id;
   Blog.findById(id)
     .then((result: any) => {
-      res.render("details", { blog: result, title: "Blog Details" });
+      res.render("blogs/details", { blog: result, title: "Blog Details" });
     })
     .catch((err: Error) => {
       console.log(err);
@@ -37,7 +37,7 @@ const blogCreatePost = (req: Request, res: Response) => {
 };
 
 const blogCreateGet = (req: Request, res: Response) => {
-  res.render("create", { title: "Create" });
+  res.render("blogs/create", { title: "Create" });
 };
 
 const blogDelete = (req: Request, res: Response) => {
